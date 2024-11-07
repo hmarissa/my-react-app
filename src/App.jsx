1355387b82baec7116738dd21bb282aadafb7252
Marissa Hart <h.marissa@wustl.edu>
2024-11-07 15:38:13 -0600

    nine buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 export default function Square() {
   return(
     <>
+    <div class="row">
     <button className="square">X</button>
     <button className="square">X</button>
+    <button className="square">X</button>
+    </div>
+    <div class="row">
+    <button className="square">X</button>
+    <button className="square">X</button>
+    <button className="square">X</button>
+    </div>
+    <div class="row">
+    <button className="square">X</button>
+    <button className="square">X</button>
+    <button className="square">X</button>
+    </div>
     </>
   ) ;
 }
